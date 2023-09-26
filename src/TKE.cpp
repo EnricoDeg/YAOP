5e91891d63dd86790133ce5f9aa25998ea32599a
Enrico Degregori <degregori@dkrz.de>
2023-09-26 21:09:44 +0200

    TKE class:  - add example use of mdspan  - template class for field type (double or float)

--- src/TKE.cpp
+++ src/TKE.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include  <experimental/mdspan>
 #include "TKE.hpp"
 
-TKE::TKE(int nproma, int nlevs, int nblocks) {
-	m_nproma = nproma;
-	m_nlevs = nlevs;
-	m_nblocks = nblocks;
+namespace stdex = std::experimental;
+
+template <class T>
+TKE<T>::TKE(int nproma, int nlevs, int nblocks) {
+    m_nproma = nproma;
+    m_nlevs = nlevs;
+    m_nblocks = nblocks;
     std::cout << "Setting nproma to " << m_nproma << std::endl;
     std::cout << "Setting nlevs to " << m_nlevs << std::endl;
     std::cout << "Setting nblocks to " << m_nblocks << std::endl;
 }
 
+template <class T>
+void TKE<T>::calc(T *temperature) {
+    m_temperature = stdex::mdspan<T, stdex::dextents<size_t, 3>>(temperature, stdex::extents{m_nblocks, m_nlevs, m_nproma});
+
+    print_field(m_temperature);
+}
+
+template <class T>
+template <
+    class ExtsA, class LayA, class AccA
+>
+void TKE<T>::print_field(
+    stdex::mdspan<T, ExtsA, LayA, AccA> a
+    ) // requires ExtsA::rank() == 3
+{
+    for(int i = 0; i < a.extent(0); ++i)
+        for(int j = 0; j < a.extent(1); ++j)
+            for(int k = 0; k < a.extent(2); ++k)
+                std::cout << "field(" << i << "," << j << "," << k << ") = " << a(i,j,k) << std::endl;
+}
+
+template class TKE<float>;
+template class TKE<double>;
--- src/TKE.hpp
+++ src/TKE.hpp
@@ -1,11 +1,28 @@
-  class TKE {
+#include  <experimental/mdspan>
+#include <iostream>
+
+
+namespace stdex = std::experimental;
+
+template<class T>
+class TKE {
     public:
         
     	TKE(int nproma, int nlevs, int nblocks);
+        void calc(T * temperature);
 
     protected:
     private:
         int m_nproma;
         int m_nlevs;
         int m_nblocks;
-  };
+        stdex::mdspan<T, stdex::dextents<size_t, 3>> m_temperature;
+
+
+        template <
+            class ExtsA, class LayA, class AccA
+        >
+        void print_field(
+        stdex::mdspan<T, ExtsA, LayA, AccA> a
+        ); // requires ExtsA::rank() == 3
+};
