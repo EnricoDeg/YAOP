9e53907e90f4ede02c4d3d74bb29f6f56334ead2
Enrico Degregori <degregori@dkrz.de>
2023-10-22 18:04:05 +0200

    examples: adapt C example to latest dev

--- examples/example_basic.c
+++ examples/example_basic.c
@@ -4,28 +4,51 @@
 
 int main(int argc, char ** argv) {
 
-    int nproma = 6;
-    int nlevs = 4;
-    int nblocks = 2;
+    int nproma = 10240;
+    int nlevs = 56;
+    int nblocks = 1;
+    int ntimesteps = 10;
+
     int block_size = nproma;
     int start_index = 0;
     int end_index = nproma - 1;
 
     TKE_Init(nproma, nlevs, nblocks, block_size, start_index, end_index);
 
-    double * temperature = malloc(nproma * nlevs * nblocks * sizeof(double));
+    double * tke = malloc(nproma * nlevs * nblocks * sizeof(double));
+    int *dolic_c = malloc(nproma * nlevs * nblocks * sizeof(int));
 
     // fill array
     for (int i=0; i<nblocks; ++i)
         for (int j=0; j<nlevs; ++j)
             for (int k=0; k<nproma; ++k)
-                temperature[k + j * nproma + i * nproma * nlevs] = 1.0 * (k + j * nproma + i * nproma * nlevs);
+                tke[k + j * nproma + i * nproma * nlevs] = 1.0 * (k + j * nproma + i * nproma * nlevs);
 
-    TKE_Calc(0,nblocks-1,temperature);
+    for (int i=0; i<nblocks; ++i)
+        for (int k=0; k<nproma; ++k)
+            dolic_c[k + i * nproma] = nlevs;
+
+    #pragma acc enter data copyin(tke[0:nproma*nlevs*nblocks-1], dolic_c[0:nproma*nblocks-1])
+
+    for (int t=0; t<ntimesteps; t++) {
+      #pragma acc host_data use_device(tke, dolic_c)
+      TKE_Calc(0, nblocks-1, tke, dolic_c);
+      
+      #pragma acc wait
+    }
+
+    #pragma acc update host(tke[0:nproma*nlevs*nblocks-1])
+
+    for (int i=0; i<nblocks; ++i)
+        for (int j=0; j<nlevs; ++j)
+            for (int k=0; k<nproma; ++k)
+                printf("tke[%d] = %e\n", k + j * nproma + i * nproma * nlevs, tke[k + j * nproma + i * nproma * nlevs]);
 
     TKE_Finalize();
 
-    free(temperature);
+    #pragma acc exit data delete(tke, dolic_c)
+    free(tke);
+    free(dolic_c);
 
     return 0;
 
