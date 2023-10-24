70e85aff773544402380c8338a14e52381c3d651
Enrico Degregori <degregori@dkrz.de>
2023-10-24 14:10:00 +0200

    data_struct: add icon like data structures

---
+++ src/data_struct.hpp
@@ -0,0 +1,14 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+// grid information
+struct t_patch {
+    int *dolic_c;
+};
+
+// cvmix parameters
+struct t_cvmix {
+    double *tke;
+};
+
+#endif /* DATA_STRUCT_HPP */
