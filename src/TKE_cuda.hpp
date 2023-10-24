2c02e55cd85ca5bcaa63f36c47f01ac03fd5302c
wiltonloch <wilton.loch@gmail.com>
2023-10-24 17:31:44 +0200

    chore: added linter requirements to src/TKE_cuda.hpp file

--- src/TKE_cuda.hpp
+++ src/TKE_cuda.hpp
@@ -14,24 +14,22 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef TKE_CUDA_HPP
-#define TKE_CUDA_HPP
+#ifndef SRC_TKE_CUDA_HPP_
+#define SRC_TKE_CUDA_HPP_
 
 #include "src/TKE_backend.hpp"
 
 class TKE_cuda : public TKE_backend {
+ public:
+    TKE_cuda(int nproma, int nlevs, int nblocks,
+             int block_size, int start_index, int end_index);
+    ~TKE_cuda();
 
-    public:
-        TKE_cuda(int nproma, int nlevs, int nblocks,
-                 int block_size, int start_index, int end_index);
-        ~TKE_cuda();
+ protected:
+    void calc_impl(int start_block, int end_block, struct t_patch p_patch, struct t_cvmix p_cvmix);
 
-    protected:
-        void calc_impl(int start_block, int end_block, struct t_patch p_patch, struct t_cvmix p_cvmix);
-
-    private:
-        bool is_view_init;
-
+ private:
+    bool is_view_init;
 };
 
-#endif /* TKE_CUDA_HPP */
+#endif  // SRC_TKE_CUDA_HPP_
