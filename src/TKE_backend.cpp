63411d898365eeecdec229f5eaebe2f15255912a
wiltonloch <wilton.loch@gmail.com>
2023-10-24 18:04:56 +0200

    chore: joined short lines in src/TKE_backend.cpp

--- src/TKE_backend.cpp
+++ src/TKE_backend.cpp
@@ -17,8 +17,7 @@
 #include "src/TKE_backend.hpp"
 #include <iostream>
 
-TKE_backend::TKE_backend(int nproma, int nlevs, int nblocks,
-                         int block_size, int start_index, int end_index)
+TKE_backend::TKE_backend(int nproma, int nlevs, int nblocks, int block_size, int start_index, int end_index)
     : m_nproma(nproma), m_nlevs(nlevs), m_nblocks(nblocks),
       m_block_size(block_size), m_start_index(start_index), m_end_index(end_index) {
 }
