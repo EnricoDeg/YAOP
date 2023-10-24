ad9e02ddcae14212956a71a407624e75bea11ba1
wiltonloch <wilton.loch@gmail.com>
2023-10-24 16:25:39 +0200

    chore: added linter requirements to example_basic.c

--- examples/example_basic.c
+++ examples/example_basic.c
@@ -19,7 +19,6 @@
 #include "TKE.h"
 
 int main(int argc, char ** argv) {
-
     int nproma = 10240;
     int nlevs = 56;
     int nblocks = 1;
@@ -74,18 +73,18 @@ int main(int argc, char ** argv) {
     double *concsum;
 
     // fill array
-    for (int i=0; i<nblocks; ++i)
-        for (int j=0; j<nlevs; ++j)
-            for (int k=0; k<nproma; ++k)
+    for (int i = 0; i < nblocks; ++i)
+        for (int j = 0; j < nlevs; ++j)
+            for (int k = 0; k < nproma; ++k)
                 tke[k + j * nproma + i * nproma * nlevs] = 1.0 * (k + j * nproma + i * nproma * nlevs);
 
-    for (int i=0; i<nblocks; ++i)
-        for (int k=0; k<nproma; ++k)
+    for (int i = 0; i < nblocks; ++i)
+        for (int k = 0; k < nproma; ++k)
             dolic_c[k + i * nproma] = nlevs;
 
     #pragma acc enter data copyin(tke[0:nproma*nlevs*nblocks-1], dolic_c[0:nproma*nblocks-1])
 
-    for (int t=0; t<ntimesteps; t++) {
+    for (int t = 0; t < ntimesteps; t++) {
       #pragma acc host_data use_device(tke, dolic_c)
       TKE_Calc(0, nblocks-1, depth_CellInterface, prism_center_dist_c,
                inv_prism_center_dist_c, prism_thick_c,
@@ -106,9 +105,9 @@ int main(int argc, char ** argv) {
 
     #pragma acc update host(tke[0:nproma*nlevs*nblocks-1])
 
-    for (int i=0; i<nblocks; ++i)
-        for (int j=0; j<nlevs; ++j)
-            for (int k=0; k<nproma; ++k)
+    for (int i = 0; i < nblocks; ++i)
+        for (int j = 0; j < nlevs; ++j)
+            for (int k = 0; k < nproma; ++k)
                 printf("tke[%d] = %e\n", k + j * nproma + i * nproma * nlevs, tke[k + j * nproma + i * nproma * nlevs]);
 
     TKE_Finalize();
@@ -118,5 +117,4 @@ int main(int argc, char ** argv) {
     free(dolic_c);
 
     return 0;
-
 }
