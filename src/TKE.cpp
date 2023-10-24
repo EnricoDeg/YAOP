adbcd25dcfbc220d93157867c926a8fe74add342
Enrico Degregori <degregori@dkrz.de>
2023-10-24 09:47:12 +0200

    TKE class: add array arguments to calc method

--- src/TKE.cpp
+++ src/TKE.cpp
@@ -30,8 +30,23 @@ TKE::~TKE() {
     delete m_impl;
 }
 
-void TKE::calc(int start_block, int end_block, double *tke, int *dolic_c) {
+void TKE::calc(int start_block, int end_block,
+               double *depth_CellInterface, double *prism_center_dist_c,
+               double *inv_prism_center_dist_c, double *prism_thick_c, 
+               int *dolic_c, int *dolic_e, double *zlev_i, double *wet_c,
+               int *edges_cell_idx, int *edges_cell_blk,
+               double *temp, double *salt, double *stretch_c, double *eta_c,
+               double *tke, double *tke_plc_in, double *hlc_in, double *wlc_in,
+               double *u_stokes_in, double *a_veloc_v, double *a_temp_v, double *a_salt_v,
+               double *iwe_Tdis, double *cvmix_dummy_1, double *cvmix_dummy_2,
+               double *cvmix_dummy_3, double *tke_Tbpr, double *tke_Tspr,
+               double *tke_Tdif, double *tke_Tdis, double *tke_Twin,
+               double *tke_Tiwf, double *tke_Tbck, double *tke_Ttot,
+               double *tke_Lmix, double *tke_Pr, double *stress_xw,
+               double *stress_yw, double *fu10, double *concsum) {
+
     m_impl->backend->calc(start_block, end_block, tke, dolic_c);
+
 }
 
 template <
