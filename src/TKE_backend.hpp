d39504f3394ba4ee28b0244e7b9246628560c9a7
wiltonloch <wilton.loch@gmail.com>
2023-10-24 17:28:26 +0200

    chore: added linter requirements to src/TKE_backend.hpp file

--- src/TKE_backend.hpp
+++ src/TKE_backend.hpp
@@ -14,50 +14,47 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef TKE_BACKEND_HPP
-#define TKE_BACKEND_HPP
+#ifndef SRC_TKE_BACKEND_HPP_
+#define SRC_TKE_BACKEND_HPP_
 
 #include <memory>
 #include "src/data_struct.hpp"
 
 class TKE_backend {
+ public:
+    typedef std::shared_ptr<TKE_backend> Ptr;
+    TKE_backend(int nproma, int nlevs, int nblocks,
+                int block_size, int start_index, int end_index);
+    virtual ~TKE_backend() = default;
+    void calc(int start_block, int end_block, struct t_patch p_patch, struct t_cvmix p_cvmix);
 
-    public:
-        typedef std::shared_ptr<TKE_backend> Ptr;
-        TKE_backend(int nproma, int nlevs, int nblocks,
-                    int block_size, int start_index, int end_index);
-        virtual ~TKE_backend() = default;
-        void calc(int start_block, int end_block, struct t_patch p_patch, struct t_cvmix p_cvmix);
+ protected:
+    virtual void calc_impl(int start_block, int end_block, struct t_patch p_patch, struct t_cvmix p_cvmix) = 0;
 
-    protected:
-        virtual void calc_impl(int start_block, int end_block, struct t_patch p_patch, struct t_cvmix p_cvmix) = 0;
+    int m_nproma;
+    int m_nlevs;
+    int m_nblocks;
+    int m_block_size;
+    int m_start_index;
+    int m_end_index;
 
-    protected:
-        int m_nproma;
-        int m_nlevs;
-        int m_nblocks;
-        int m_block_size;
-        int m_start_index;
-        int m_end_index;
-
-        double *m_rho_up;
-        double *m_rho_down;
-        double *m_tke_old;
-        double *m_tke_Av;
-        double *m_tke_kv;
-        double *m_tke_iw_alpha_c;
-        double *m_tke_iwe;
-        double *m_tke_iwe_forcing;
-        double *m_forc_tke_surf_2D;
-        double *m_forc_rho_surf_2D;
-        double *m_bottom_fric_2D;
-        double *m_s_c;
-        double *m_dzw_stretched;
-        double *m_dzt_stretched;
-        double *m_pressure;
-        double *m_Nsqr;
-        double *m_Ssqr;
-
+    double *m_rho_up;
+    double *m_rho_down;
+    double *m_tke_old;
+    double *m_tke_Av;
+    double *m_tke_kv;
+    double *m_tke_iw_alpha_c;
+    double *m_tke_iwe;
+    double *m_tke_iwe_forcing;
+    double *m_forc_tke_surf_2D;
+    double *m_forc_rho_surf_2D;
+    double *m_bottom_fric_2D;
+    double *m_s_c;
+    double *m_dzw_stretched;
+    double *m_dzt_stretched;
+    double *m_pressure;
+    double *m_Nsqr;
+    double *m_Ssqr;
 };
 
-#endif /* TKE_BACKEND_HPP */
+#endif  // SRC_TKE_BACKEND_HPP_
