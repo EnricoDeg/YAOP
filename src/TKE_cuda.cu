73becb6e5788cc6f5841b16d9197e355628ea756
Enrico Degregori <degregori@dkrz.de>
2023-11-03 18:16:26 +0100

    CUDA: kernel implementation
     - start pre-intergration
    
     - device function to calculate density

--- src/TKE_cuda.cu
+++ src/TKE_cuda.cu
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
 #include <iostream>
 #include "src/TKE_cuda.hpp"
 #include "src/utils.hpp"
@@ -30,6 +31,7 @@ struct t_constant {
     double clc;
     double ReferencePressureIndbars;
     double pi;
+    int nlevs;
 };
 
 // Structures with memory views
@@ -48,6 +50,9 @@ __global__ void calc_impl_kernel(int blockNo, int start_index, int end_index,
                                  t_ocean_state_view ocean_state, t_atmo_fluxes_view atmos_fluxes,
                                  t_atmos_for_ocean_view p_as, t_sea_ice_view p_sea_ice,
                                  t_tke_internal_view p_internal, t_constant p_constant);
+
+__device__
+double  calculate_density(double temp, double salt, double pressure);
 
 TKE_cuda::TKE_cuda(int nproma, int nlevs, int nblocks, int vert_mix_type, int vmix_idemix_tke,
                    int vert_cor_type, double dtime, double OceanReferenceDensity, double grav,
@@ -127,6 +132,7 @@ void TKE_cuda::calc_impl(t_patch p_patch, t_cvmix p_cvmix,
         p_constant.clc = m_clc;
         p_constant.ReferencePressureIndbars = m_ReferencePressureIndbars;
         p_constant.pi = m_pi;
+        p_constant.nlevs = m_nlevs;
         is_view_init = true;
     }
 
@@ -154,7 +160,7 @@ void calc_impl_kernel(int blockNo, int start_index, int end_index, t_patch_view 
                       t_tke_internal_view p_internal, t_constant p_constant) {
     int jc = blockIdx.x * blockDim.x + threadIdx.x + start_index;
     if (jc <= end_index) {
-        int levels = p_patch.dolic_c(blockNo, jc);
+        int levels = p_constant.nlevs;
         // initialization
         for (int level = 0; level < levels+1; level++) {
             p_internal.tke_kv(level, jc) = 0.0;
@@ -178,9 +184,122 @@ void calc_impl_kernel(int blockNo, int start_index, int end_index, t_patch_view 
             p_internal.pressure(level, jc) = p_patch.zlev_i(level) * p_constant.ReferencePressureIndbars;
 
         // pre-integration
+        levels = p_patch.dolic_c(blockNo, jc);
+        if (levels > 0) {
+            for (int level = 0; level < levels+1; level++) {
+                p_internal.tke_old(level, jc) = p_cvmix.tke(blockNo, level, jc);
+                p_internal.Nsqr(level, jc) = 0.0;
+                p_internal.Ssqr(level, jc) = 0.0;
+            }
+            for (int level = 0; level < levels; level++) {
+                p_internal.rho_up(level, jc) = 0.0;
+                p_internal.rho_down(level, jc) = 0.0;
+            }
+            double tau_abs = (1.0 - p_sea_ice.concsum(blockNo, jc))
+                             * sqrt(pow(atmos_fluxes.stress_xw(blockNo, jc), 2.0)
+                                  + pow(atmos_fluxes.stress_yw(blockNo, jc), 2.0));
+            p_internal.forc_tke_surf_2D(jc) = tau_abs / p_constant.OceanReferenceDensity;
+        }
 
         // integration
 
         // post-integration
     }
+}
+
+#define a_c3 2.1987e-10
+#define a_a3 5.4065e-7
+#define a_a4 4.0274e-9
+#define a_b1 1.7439E-5
+#define a_d 4.1057e-9
+#define z_sref 35.0
+#define a_a1 3.6504e-4
+#define a_c1 8.9309e-7
+#define a_e1 1.6056e-10
+#define a_b2 2.9778e-7
+#define a_a2 8.3198e-5
+#define a_c2 3.1628e-8
+#define a_e2 5.0484e-12
+#define r_a0 999.842594
+#define r_a1 6.793952e-2
+#define r_a2 -9.095290e-3
+#define r_a3 1.001685e-4
+#define r_a4 -1.120083e-6
+#define r_a5 6.536332e-9
+#define r_b0 8.24493e-1
+#define r_b1 -4.0899e-3
+#define r_b2 7.6438e-5
+#define r_b3 -8.2467e-7
+#define r_b4 5.3875e-9
+#define r_c0 -5.72466e-3
+#define r_c1 1.0227e-4
+#define r_c2 -1.6546e-6
+#define r_d0 4.8314e-4
+#define r_h0 3.239908
+#define r_h1 1.43713e-3
+#define r_h2 1.16092e-4
+#define r_h3 -5.77905e-7
+#define r_ai0 2.2838e-3
+#define r_ai1 -1.0981e-5
+#define r_ai2 -1.6078e-6
+#define r_aj0 1.91075e-4
+#define r_ak0 8.50935e-5
+#define r_ak1 -6.12293e-6
+#define r_ak2 5.2787e-8
+#define r_am0 -9.9348e-7
+#define r_am1 2.0816e-8
+#define r_am2 9.1697e-10
+#define r_e0 19652.21
+#define r_e1 148.4206
+#define r_e2 -2.327105
+#define r_e3 1.360477e-2
+#define r_e4 -5.155288e-5
+#define r_f0 54.6746
+#define r_f1 -0.603459
+#define r_f2 1.09987e-2
+#define r_f3 -6.1670e-5
+#define r_g0 7.944e-2
+#define r_g1 1.6483e-2
+#define r_g2 -5.3009e-4
+
+__device__
+double  calculate_density(double temp, double salt, double pressure) {
+    double dvs, fne, fst, qn3;
+    double qnq, qvs, s, s3h;
+    double t, denom, s__2;
+    double rho;
+
+    // This is the adisit part, that transforms potential in in-situ temperature
+    qnq = -pressure * (-a_a3 + pressure * a_c3);
+    qn3 = -pressure * a_a4;
+    qvs = (pressure * (a_b1 - a_d * pressure)) *
+          (salt - z_sref) + pressure * (a_a1 + pressure * (a_c1 - a_e1 * pressure));
+
+    dvs = (a_b2 * pressure) * (salt - z_sref) +
+           1.0 + pressure * (-a_a2 + pressure * (a_c2 - a_e2 * pressure));
+
+    t   = (temp + qvs) / dvs;
+    fne = - qvs + t * (dvs + t * (qnq + t * qn3)) - temp;
+
+    fst = dvs + t * (2.0 * qnq + 3.0 * qn3 * t);
+
+    t    = t - fne / fst;
+    s    = max(salt, 0.0);
+    s__2 = pow(s, 2.0);
+    s3h  = s * sqrt(s);
+
+    rho = r_a0 + t * (r_a1 + t * (r_a2 + t * (r_a3 + t * (r_a4 + t * r_a5))))
+        + s * (r_b0 + t * (r_b1 + t * (r_b2 + t * (r_b3 + t * r_b4))))
+        + r_d0 * s__2 + s3h * (r_c0 + t * (r_c1 + r_c2 * t));
+
+    denom = 1.0 - pressure / (pressure * (r_h0 + t *
+            (r_h1 + t * (r_h2 + t * r_h3))
+            + s * (r_ai0 + t * (r_ai1 + r_ai2 * t))
+            + r_aj0 * s3h + (r_ak0 + t * (r_ak1 + t * r_ak2)
+            + s * (r_am0 + t * (r_am1 + t * r_am2))) * pressure)
+            + r_e0 + t * (r_e1 + t * (r_e2 + t * (r_e3 + t * r_e4)))
+            + s * (r_f0 + t * (r_f1 + t * (r_f2 + t * r_f3)))
+            + s3h * (r_g0 + t * (r_g1 + r_g2 * t)));
+
+    return rho/denom;
 }
