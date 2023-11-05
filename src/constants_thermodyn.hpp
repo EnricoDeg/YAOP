8000b23b679588e33b216df4a9ddcfec2b023813
Enrico Degregori <degregori@dkrz.de>
2023-11-05 11:00:53 +0100

    CUDA: move thermodyn constants in header file

---
+++ src/constants_thermodyn.hpp
@@ -0,0 +1,75 @@
+/* Copyright (C) 2023  Enrico Degregori, Wilton Jaciel Loch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_CONSTANTS_THERMODYN_HPP_
+#define SRC_CONSTANTS_THERMODYN_HPP_
+
+#define a_c3 2.1987e-10
+#define a_a3 5.4065e-7
+#define a_a4 4.0274e-9
+#define a_b1 1.7439E-5
+#define a_d 4.1057e-9
+#define z_sref 35.0
+#define a_a1 3.6504e-4
+#define a_c1 8.9309e-7
+#define a_e1 1.6056e-10
+#define a_b2 2.9778e-7
+#define a_a2 8.3198e-5
+#define a_c2 3.1628e-8
+#define a_e2 5.0484e-12
+#define r_a0 999.842594
+#define r_a1 6.793952e-2
+#define r_a2 -9.095290e-3
+#define r_a3 1.001685e-4
+#define r_a4 -1.120083e-6
+#define r_a5 6.536332e-9
+#define r_b0 8.24493e-1
+#define r_b1 -4.0899e-3
+#define r_b2 7.6438e-5
+#define r_b3 -8.2467e-7
+#define r_b4 5.3875e-9
+#define r_c0 -5.72466e-3
+#define r_c1 1.0227e-4
+#define r_c2 -1.6546e-6
+#define r_d0 4.8314e-4
+#define r_h0 3.239908
+#define r_h1 1.43713e-3
+#define r_h2 1.16092e-4
+#define r_h3 -5.77905e-7
+#define r_ai0 2.2838e-3
+#define r_ai1 -1.0981e-5
+#define r_ai2 -1.6078e-6
+#define r_aj0 1.91075e-4
+#define r_ak0 8.50935e-5
+#define r_ak1 -6.12293e-6
+#define r_ak2 5.2787e-8
+#define r_am0 -9.9348e-7
+#define r_am1 2.0816e-8
+#define r_am2 9.1697e-10
+#define r_e0 19652.21
+#define r_e1 148.4206
+#define r_e2 -2.327105
+#define r_e3 1.360477e-2
+#define r_e4 -5.155288e-5
+#define r_f0 54.6746
+#define r_f1 -0.603459
+#define r_f2 1.09987e-2
+#define r_f3 -6.1670e-5
+#define r_g0 7.944e-2
+#define r_g1 1.6483e-2
+#define r_g2 -5.3009e-4
+
+#endif  // SRC_CONSTANTS_THERMODYN_HPP_
