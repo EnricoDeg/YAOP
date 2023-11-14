012c1068472fbadb3677f60440f85f54d8f27260
Enrico Degregori <degregori@dkrz.de>
2023-11-14 10:33:57 +0100

    Refactor: move cuda kernels and memory functions in separate files

---
+++ src/cuda_kernels.hpp
@@ -0,0 +1,46 @@
+/* Copyright (C) 2023  Enrico Degregori, Wilton Jaciel Loch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_CUDA_KERNELS_HPP_
+#define SRC_CUDA_KERNELS_HPP_
+
+#include "src/cuda_memory.hpp"
+
+__global__ void calc_impl_cells(int blockNo, int start_index, int end_index,
+                                 t_patch_view p_patch, t_cvmix_view p_cvmix,
+                                 t_ocean_state_view ocean_state, t_atmo_fluxes_view atmos_fluxes,
+                                 t_atmos_for_ocean_view p_as, t_sea_ice_view p_sea_ice,
+                                 t_tke_internal_view p_internal, t_constant p_constant,
+                                 t_constant_tke p_constant_tke);
+
+__global__
+void calc_impl_edges(int blockNo, int start_index, int end_index, t_patch_view p_patch,
+                      t_cvmix_view p_cvmix, t_tke_internal_view p_internal, t_constant p_constant);
+
+__device__
+void integrate(int jc, int nlevels, int blockNo, t_patch_view p_patch, t_cvmix_view p_cvmix,
+               t_tke_internal_view p_internal, t_constant p_constant,
+               t_constant_tke p_constant_tke);
+
+__device__
+void solve_tridiag(int jc, int nlevels, int blockNo, mdspan_2d_double a,
+                   mdspan_2d_double b, mdspan_2d_double c, mdspan_2d_double d,
+                   mdspan_3d_double x, mdspan_2d_double cp, mdspan_2d_double dp);
+
+__device__
+double  calculate_density(double temp, double salt, double pressure);
+
+#endif  // SRC_CUDA_KERNELS_HPP_
--- src/cuda_memory.hpp
+++ src/cuda_memory.hpp
@@ -20,6 +20,7 @@
 #include <cuda.h>
 #include <cuda/std/mdspan>
 #include "src/cuda_check.hpp"
+#include "src/data_struct.hpp"
 
 constexpr auto dyn = cuda::std::dynamic_extent;
 using ext1d_t = cuda::std::extents<size_t, dyn>;
@@ -30,6 +31,41 @@ using mdspan_2d_double = cuda::std::mdspan<double, ext2d_t>;
 using mdspan_3d_double = cuda::std::mdspan<double, ext3d_t>;
 using mdspan_2d_int = cuda::std::mdspan<int, ext2d_t>;
 using mdspan_3d_int = cuda::std::mdspan<int, ext3d_t>;
+
+// TKE constants
+struct t_constant {
+    int vert_mix_type;
+    int vmix_idemix_tke;
+    int vert_cor_type;
+    double dtime;
+    double OceanReferenceDensity;
+    double grav;
+    int l_lc;
+    double clc;
+    double ReferencePressureIndbars;
+    double pi;
+    int nlevs;
+};
+
+struct t_constant_tke {
+    double c_k;
+    double c_eps;
+    double cd;
+    double alpha_tke;
+    double clc;
+    double mxl_min;
+    double KappaM_min;
+    double KappaH_min;
+    double KappaM_max;
+    double tke_surf_min;
+    double tke_min;
+    int tke_mxl_choice;
+    int handle_old_vals;
+    bool only_tke;
+    bool use_Kappa_min;
+    bool use_ubound_dirichlet;
+    bool use_lbound_dirichlet;
+};
 
 // TKE internal memory views
 struct t_tke_internal_view {
@@ -136,92 +172,28 @@ struct t_sea_ice_view {
 };
 
 void fill_struct_view(struct t_cvmix_view *p_cvmix_view, struct t_cvmix *p_cvmix,
-                             int nblocks, int nlevs, int nproma) {
-    p_cvmix_view->tke = mdspan_3d_double{ p_cvmix->tke, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_plc = mdspan_3d_double{ p_cvmix->tke_plc, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->hlc = mdspan_2d_double{ p_cvmix->hlc, ext2d_t{nblocks, nproma} };
-    p_cvmix_view->wlc = mdspan_3d_double{ p_cvmix->wlc, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->u_stokes = mdspan_2d_double{ p_cvmix->u_stokes, ext2d_t{nblocks, nproma} };
-    p_cvmix_view->a_veloc_v = mdspan_3d_double{ p_cvmix->a_veloc_v, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->a_temp_v = mdspan_3d_double{ p_cvmix->a_temp_v, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->a_salt_v = mdspan_3d_double{ p_cvmix->a_salt_v, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->iwe_Tdis = mdspan_3d_double{ p_cvmix->iwe_Tdis, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->cvmix_dummy_1 = mdspan_3d_double{ p_cvmix->cvmix_dummy_1, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->cvmix_dummy_2 = mdspan_3d_double{ p_cvmix->cvmix_dummy_2, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->cvmix_dummy_3 = mdspan_3d_double{ p_cvmix->cvmix_dummy_3, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Tbpr = mdspan_3d_double{ p_cvmix->tke_Tbpr, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Tspr = mdspan_3d_double{ p_cvmix->tke_Tspr, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Tdif = mdspan_3d_double{ p_cvmix->tke_Tdif, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Tdis = mdspan_3d_double{ p_cvmix->tke_Tdis, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Twin = mdspan_3d_double{ p_cvmix->tke_Twin, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Tiwf = mdspan_3d_double{ p_cvmix->tke_Tiwf, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Tbck = mdspan_3d_double{ p_cvmix->tke_Tbck, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Ttot = mdspan_3d_double{ p_cvmix->tke_Ttot, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Lmix = mdspan_3d_double{ p_cvmix->tke_Lmix, ext3d_t{nblocks, nlevs+1, nproma} };
-    p_cvmix_view->tke_Pr = mdspan_3d_double{ p_cvmix->tke_Pr, ext3d_t{nblocks, nlevs+1, nproma} };
-}
+                             int nblocks, int nlevs, int nproma);
 
 void fill_struct_view(struct t_patch_view *p_patch_view, struct t_patch *p_patch,
-                             int nblocks, int nlevs, int nproma) {
-    p_patch_view->depth_CellInterface = mdspan_3d_double{ p_patch->depth_CellInterface,
-                                                          ext3d_t{nblocks, nlevs+1, nproma} };
-    p_patch_view->prism_center_dist_c = mdspan_3d_double{ p_patch->prism_center_dist_c,
-                                                          ext3d_t{nblocks, nlevs+1, nproma} };
-    p_patch_view->inv_prism_center_dist_c = mdspan_3d_double{ p_patch->inv_prism_center_dist_c,
-                                                              ext3d_t{nblocks, nlevs+1, nproma} };
-    p_patch_view->prism_thick_c = mdspan_3d_double{ p_patch->prism_thick_c, ext3d_t{nblocks, nlevs, nproma} };
-    p_patch_view->dolic_c = mdspan_2d_int{ p_patch->dolic_c, ext2d_t{nblocks, nproma} };
-    p_patch_view->dolic_e = mdspan_2d_int{ p_patch->dolic_e, ext2d_t{nblocks, nproma} };
-    p_patch_view->zlev_i = mdspan_1d_double{ p_patch->zlev_i, ext1d_t{nlevs} };
-    p_patch_view->wet_c = mdspan_3d_double{ p_patch->wet_c, ext3d_t{nblocks, nlevs, nproma} };
-    p_patch_view->edges_cell_idx = mdspan_3d_int{ p_patch->edges_cell_idx, ext3d_t{2, nlevs, nproma} };
-    p_patch_view->edges_cell_blk = mdspan_3d_int{ p_patch->edges_cell_blk, ext3d_t{2, nlevs, nproma} };
-}
+                             int nblocks, int nlevs, int nproma);
 
 
-static void fill_struct_view(struct t_ocean_state_view *ocean_state_view, struct t_ocean_state *ocean_state,
-                             int nblocks, int nlevs, int nproma) {
-    ocean_state_view->temp = mdspan_3d_double{ ocean_state->temp, ext3d_t{nblocks, nlevs, nproma} };
-    ocean_state_view->salt = mdspan_3d_double{ ocean_state->salt, ext3d_t{nblocks, nlevs, nproma} };
-    ocean_state_view->stretch_c = mdspan_2d_double{ ocean_state->stretch_c, ext2d_t{nblocks, nproma} };
-    ocean_state_view->eta_c = mdspan_2d_double{ ocean_state->eta_c, ext2d_t{nblocks, nproma} };
-    ocean_state_view->p_vn_x1 = mdspan_3d_double{ ocean_state->p_vn_x1, ext3d_t{nblocks, nlevs, nproma} };
-    ocean_state_view->p_vn_x2 = mdspan_3d_double{ ocean_state->p_vn_x2, ext3d_t{nblocks, nlevs, nproma} };
-    ocean_state_view->p_vn_x3 = mdspan_3d_double{ ocean_state->p_vn_x3, ext3d_t{nblocks, nlevs, nproma} };
-}
+void fill_struct_view(struct t_ocean_state_view *ocean_state_view, struct t_ocean_state *ocean_state,
+                             int nblocks, int nlevs, int nproma);
 
-static void fill_struct_view(struct t_atmo_fluxes_view *atmos_fluxes_view, struct t_atmo_fluxes *atmos_fluxes,
-                             int nblocks, int nlevs, int nproma) {
-    atmos_fluxes_view->stress_xw = mdspan_2d_double{ atmos_fluxes->stress_xw, ext2d_t{nblocks, nproma} };
-    atmos_fluxes_view->stress_yw = mdspan_2d_double{ atmos_fluxes->stress_yw, ext2d_t{nblocks, nproma} };
-}
+void fill_struct_view(struct t_atmo_fluxes_view *atmos_fluxes_view, struct t_atmo_fluxes *atmos_fluxes,
+                             int nblocks, int nlevs, int nproma);
 
-static void fill_struct_view(struct t_atmos_for_ocean_view *p_as_view, struct t_atmos_for_ocean *p_as,
-                             int nblocks, int nlevs, int nproma) {
-    p_as_view->fu10 = mdspan_2d_double{ p_as->fu10, ext2d_t{nblocks, nproma} };
-}
+void fill_struct_view(struct t_atmos_for_ocean_view *p_as_view, struct t_atmos_for_ocean *p_as,
+                             int nblocks, int nlevs, int nproma);
 
-static void fill_struct_view(struct t_sea_ice_view *p_sea_ice_view, struct t_sea_ice *p_sea_ice,
-                             int nblocks, int nlevs, int nproma) {
-    p_sea_ice_view->concsum = mdspan_2d_double{ p_sea_ice->concsum, ext2d_t{nblocks, nproma} };
-}
+void fill_struct_view(struct t_sea_ice_view *p_sea_ice_view, struct t_sea_ice *p_sea_ice,
+                             int nblocks, int nlevs, int nproma);
 
-mdspan_1d_double view_cuda_malloc(double *field, size_t dim1) {
-    check( cudaMalloc(&field, dim1*sizeof(double)) );
-    mdspan_1d_double memview{ field, ext1d_t{dim1} };
-    return memview;
-}
+mdspan_1d_double view_cuda_malloc(double *field, size_t dim1);
 
-mdspan_2d_double view_cuda_malloc(double *field, size_t dim1, size_t dim2) {
-    check( cudaMalloc(&field, dim1*dim2*sizeof(double)) );
-    mdspan_2d_double memview{ field, ext2d_t{dim1, dim2} };
-    return memview;
-}
+mdspan_2d_double view_cuda_malloc(double *field, size_t dim1, size_t dim2);
 
-mdspan_3d_double view_cuda_malloc(double *field, size_t dim1, size_t dim2, size_t dim3) {
-    check( cudaMalloc(&field, dim1*dim2*dim3*sizeof(double)) );
-    mdspan_3d_double memview{ field, ext3d_t{dim1, dim2, dim3} };
-    return memview;
-}
+mdspan_3d_double view_cuda_malloc(double *field, size_t dim1, size_t dim2, size_t dim3);
 
 #endif  // SRC_CUDA_MEMORY_HPP_
