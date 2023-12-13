13ce69a96c064e914c887c04ca56aaeb34cd65d5
Enrico Degregori <degregori@dkrz.de>
2023-12-13 16:38:43 +0100

    doxygen: document the functions in data_struct.hpp file

--- src/shared/interface/data_struct.hpp
+++ src/shared/interface/data_struct.hpp
@@ -115,23 +115,41 @@ struct t_sea_ice {
     double *concsum;
 };
 
+/*! \brief Fill grid info data struct from array pointers.
+*
+*/
 void fill_struct(struct t_patch *p_patch, double *depth_CellInterface, double *prism_center_dist_c,
                  double *inv_prism_center_dist_c, double *prism_thick_c, int *dolic_c, int *dolic_e,
                  double *zlev_i, double *wet_c, int *edges_cell_idx, int *edges_cell_blk);
 
+/*! \brief Fill cvmix data struct from array pointers.
+*
+*/
 void fill_struct(struct t_cvmix * p_cvmix, double *tke, double *tke_plc, double *hlc, double *wlc,
                  double *u_stokes, double *a_veloc_v, double *a_temp_v, double *a_salt_v, double *iwe_Tdis,
                  double *cvmix_dummy_1, double *cvmix_dummy_2, double *cvmix_dummy_3, double *tke_Tbpr,
                  double *tke_Tspr, double *tke_Tdif, double *tke_Tdis, double *tke_Twin, double *tke_Tiwf,
                  double *tke_Tbck, double *tke_Ttot, double *tke_Lmix, double *tke_Pr);
 
+/*! \brief Fill ocean state data struct from array pointers.
+*
+*/
 void fill_struct(struct t_ocean_state * ocean_state, double *temp, double *salt, double *stretch_c,
                  double *eta_c, double *p_vn_x1, double *p_vn_x2, double *p_vn_x3);
 
+/*! \brief Fill atmosphere fluxes data struct from array pointers.
+*
+*/
 void fill_struct(struct t_atmo_fluxes *atmo_fluxes, double *stress_xw, double *stress_yw);
 
+/*! \brief Fill atmosphere data struct from array pointers.
+*
+*/
 void fill_struct(struct t_atmos_for_ocean *p_as, double *fu10);
 
+/*! \brief Fill sea ice data struct from array pointers.
+*
+*/
 void fill_struct(struct t_sea_ice *p_sea_ice, double *concsum);
 
 #endif  // SRC_SHARED_INTERFACE_DATA_STRUCT_HPP_
