4355dcbf0b29c0d6a1da8d6e4c8b608ab80c8f93
Enrico Degregori <degregori@dkrz.de>
2023-12-13 14:08:32 +0100

    doxygen: document TKE_cpu class

--- src/backends/CPU/TKE_cpu.hpp
+++ src/backends/CPU/TKE_cpu.hpp
@@ -19,14 +19,32 @@
 
 #include "src/backends/TKE_backend.hpp"
 
+/*! \brief TKE CPU backend class, derived from TKE_backend.
+ *
+ */
 class TKE_cpu : public TKE_backend {
  public:
+    /*! \brief TKE_cpu class constructor.
+    *
+    *   It calls the TKE_backend constructor and the internal_fields_malloc method with
+    *   specific memory view and policy.
+    */
     TKE_cpu(int nproma, int nlevs, int nblocks, int vert_mix_type, int vmix_idemix_tke,
              int vert_cor_type, double dtime, double OceanReferenceDensity, double grav,
              int l_lc, double clc, double ReferencePressureIndbars, double pi);
+
+    /*! \brief TKE_cpu class destructor.
+    *
+    *   It calls internal_fields_free with specific memory view policy.
+    */
     ~TKE_cpu();
 
  protected:
+    /*! \brief CPU implementation of TKE.
+    *
+    *   It fills the memory view structures during the first call and then compute the
+    *   turbulent kinetic energy vertical scheme.
+    */
     void calc_impl(struct t_patch p_patch, struct t_cvmix p_cvmix,
                    struct t_ocean_state ocean_state, struct t_atmo_fluxes atmos_fluxes,
                    struct t_atmos_for_ocean p_as, struct t_sea_ice p_sea_ice,
