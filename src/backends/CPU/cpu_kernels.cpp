a9cf611f633045ac417d4619653c05906b2c5323
Enrico Degregori <degregori@dkrz.de>
2023-12-15 10:50:14 +0100

    CPU: implement mixing length scale calculation function

--- src/backends/CPU/cpu_kernels.cpp
+++ src/backends/CPU/cpu_kernels.cpp
@@ -20,6 +20,9 @@
 #include "src/backends/kernels.hpp"
 #include "src/shared/constants/constants_thermodyn.hpp"
 
+using std::max;
+using std::min;
+
 void calc_impl_cells(int blockNo, int start_index, int end_index,
                      t_patch_view<cpu_memview::mdspan, cpu_memview::dextents> p_patch,
                      t_cvmix_view<cpu_memview::mdspan, cpu_memview::dextents> p_cvmix,
@@ -112,12 +115,70 @@ void calc_impl_cells(int blockNo, int start_index, int end_index,
                                                   ocean_state.stretch_c(blockNo, jc);
 }
 
-void integrate(int jc, int blockNo,
+void integrate(int blockNo, int start_index, int end_index,
                t_patch_view<cpu_memview::mdspan, cpu_memview::dextents> p_patch,
                t_cvmix_view<cpu_memview::mdspan, cpu_memview::dextents> p_cvmix,
                t_tke_internal_view<cpu_memview::mdspan, cpu_memview::dextents> p_internal,
                t_constant p_constant,
                t_constant_tke p_constant_tke) {
+    // compute max level on block (maxval fortran function)
+    int max_levels = 0;
+    for (int jc = start_index; jc <= end_index; jc++)
+        if (p_patch.dolic_c(blockNo, jc) > max_levels)
+            max_levels = p_patch.dolic_c(blockNo, jc);
+
+    // Initialize diagnostics and calculate mixing length scale
+    for (int level = 0; level < p_constant.nlevs+1; level++) {
+        for (int jc = start_index; jc <= end_index; jc++) {
+            p_cvmix.tke_Twin(blockNo, level, jc) = 0.0;
+            p_internal.sqrttke(level, jc) = sqrt(max(0.0, p_internal.tke_old(level, jc)));
+            p_cvmix.tke_Lmix(blockNo, level, jc) = sqrt(2.0) * p_internal.sqrttke(level, jc) /
+                                    sqrt(max(1.0e-12, p_internal.Nsqr(level, jc)));
+        }
+    }
+
+    if (p_constant_tke.tke_mxl_choice == 2) {
+        calculate_mxl_2(blockNo, start_index, end_index, max_levels, p_constant_tke.mxl_min,
+                        p_patch.dolic_c, p_cvmix.tke_Lmix, p_internal.dzw_stretched);
+    } else if (p_constant_tke.tke_mxl_choice == 3) {
+        // TODO(EnricoDeg): not default
+    } else {
+        // Error
+    }
+}
+
+void calculate_mxl_2(int blockNo, int start_index, int end_index, int max_levels, double mxl_min,
+                     mdspan_2d_int dolic_c, mdspan_3d_double tke_Lmix, mdspan_2d_double dzw_stretched) {
+    for (int jc = start_index; jc <= end_index; jc++) {
+        if (dolic_c(blockNo, jc) > 0) {
+            tke_Lmix(blockNo, 0, jc) = 0.0;
+            tke_Lmix(blockNo, dolic_c(blockNo, jc), jc) = 0.0;
+        }
+    }
+
+    for (int level = 1; level < max_levels; level++)
+        for (int jc = start_index; jc <= end_index; jc++)
+            if (level < dolic_c(blockNo, jc))
+                tke_Lmix(blockNo, level, jc) = min(tke_Lmix(blockNo, level, jc),
+                         tke_Lmix(blockNo, level-1, jc) + dzw_stretched(level-1, jc));
+
+    for (int jc = start_index; jc <= end_index; jc++)
+        if (dolic_c(blockNo, jc) > 0) {
+            int dolic = dolic_c(blockNo, jc);
+            tke_Lmix(blockNo, dolic-1, jc) = min(tke_Lmix(blockNo, dolic-1, jc),
+                                             mxl_min + dzw_stretched(dolic-1, jc));
+        }
+
+    for (int level = max_levels-2; level > 0; level--)
+        for (int jc = start_index; jc <= end_index; jc++)
+            if (level < dolic_c(blockNo, jc) - 1)
+                tke_Lmix(blockNo, level, jc) = min(tke_Lmix(blockNo, level, jc),
+                         tke_Lmix(blockNo, level+1, jc) +  dzw_stretched(level, jc));
+
+    for (int level = 0; level < max_levels+1; level++)
+        for (int jc = start_index; jc <= end_index; jc++)
+            if (level < dolic_c(blockNo, jc) + 1)
+                tke_Lmix(blockNo, level, jc) = max(tke_Lmix(blockNo, level, jc), mxl_min);
 }
 
 void calc_impl_edges(int blockNo, int start_index, int end_index,
